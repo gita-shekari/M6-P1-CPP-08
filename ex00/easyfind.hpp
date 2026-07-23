54a7ea6d1dab8be314f10401932dd759269e535c
Gita Shekari <gshekari@f1r1s6.codam.nl>
2026-07-23 20:17:35 +0200

    update

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -2,9 +2,19 @@
 #ifndef EASYFIND_HPP
 #define EASYFIND_HPP
 
+#include <algorithm>
+#include <iostream>
+#include <exception>
+
 template<typename T>
-T* easyFind(T& data, int num)
+typename T::iterator easyFind(T& data, int num)
 {
-	for(size_t i = 0; i <  )
+	typename T::iterator it;
+	it = std::find(data.begin(), data.end(), num);
+	if (it == data.end())
+	{
+		throw std::runtime_error("Not found");
+	}
+	return it;
 }
 #endif
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,5 +1,74 @@
 
+#include "easyfind.hpp"
+#include <vector>
+#include <list>
+#include <deque>
+
 int main()
 {
+	std::cout << "\n--- testing vector ---\n";
+	std::vector<int> numbers_vector;
+	numbers_vector.push_back(10);
+	numbers_vector.push_back(20);
+	numbers_vector.push_back(30);
+	try
+	{
+		std::cout << *(easyFind(numbers_vector, 20)) << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	try
+	{
+		std::cout << *(easyFind(numbers_vector, 0)) << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	std::cout << "\n--- tesing list ---\n";
+	std::list<int> numbers_list;
+	numbers_list.push_back(1);
+	numbers_list.push_back(2);
+	numbers_list.push_back(3);
+	try
+	{
+		std::cout << *(easyFind(numbers_list, 1)) << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	try
+	{
+		std::cout << *(easyFind(numbers_list, 0)) << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+
+	std::cout << "\n--- tesing deque---\n";
+	std::deque<int> numbers_deque;
+	numbers_deque.push_back(1);
+	numbers_deque.push_back(2);
+	numbers_deque.push_back(3);
+	try
+	{
+		std::cout << *(easyFind(numbers_deque, 3)) << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	try
+	{
+		std::cout << *(easyFind(numbers_deque, 0)) << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
 	return 0;
 }
