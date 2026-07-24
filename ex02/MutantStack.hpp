6546de5d987776aa31824bf31651738ebee340d7
Gita Shekari <gshekari@f1r1s2.codam.nl>
2026-07-24 21:00:31 +0200

    update

---
+++ ex02/MutantStack.hpp
@@ -0,0 +1,23 @@
+
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
+
+#include <algorithm>
+#include <iostream>
+#include <exception>
+#include <stack>
+
+template <typename T>
+class MutantStack : public std::stack<T>
+{
+	private:
+
+	public:
+		MutantStack();
+		MutantStack(unsigned int n);
+		MutantStack(const MutantStack &other);
+		MutantStack &operator=(const MutantStack &other);
+		~MutantStack();
+};
+
+#endif
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,11 +1,28 @@
-#include "Span.hpp"
-#include <climits>
-#include <ctime>
-#include <cstdlib>
+#include ".hpp"
+
 
 int main()
 {
-	std::cout << "\n--- subject test---\n";
-
+	MutantStack<int> mstack;
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << mstack.size() << std::endl;
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	//[...]
+	mstack.push(0);
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	++it;
+	--it;
+	while (it != ite)
+	{
+	std::cout << *it << std::endl;
+	++it;
+	}
+	std::stack<int> s(mstack);
 	return 0;
 }
